95bcb34d4b64f6d9fd0326c36ebd0907a0409023
bardiniww <bardiniww@gmail.com>
2023-01-22 18:32:07 +0800

    initial

---
+++ src/test/java/com/bardiniww/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bardiniww.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
