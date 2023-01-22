c70c23a3a5d972dfbdb2e9442b7dbff63397c945
bardiniww <bardiniww@gmail.com>
2023-01-22 20:28:19 +0800

    claims extract methods implemented

--- src/main/java/com/bardiniww/security/config/JwtService.java
+++ src/main/java/com/bardiniww/security/config/JwtService.java
@@ -1,13 +1,42 @@
 package com.bardiniww.security.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
 
 @Service
 @AllArgsConstructor
 public class JwtService {
 
+    // https://www.allkeysgenerator.com/Random/Security-Encryption-Key-Generator.aspx
+    private static final String SECRET_KEY = "25432A462D4A614E645267556B586E3272357538782F413F4428472B4B625065";
+
     public String extractUserName(String token) {
-        return null;
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSighingKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSighingKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
